78239a6ce1991ebd9534c0b0786c937e65187796
MoinulAx <113442311+MoinulAx@users.noreply.github.com>
2022-11-15 10:50:05 -0500

    todo

---
+++ list.py
@@ -0,0 +1,42 @@
+
+mylist=["meat","beep",'boop',"meep"]
+
+
+print(mylist)
+
+mylist.append ("ginger")
+
+print()
+
+print(mylist)
+
+print()
+
+print (mylist[2])
+
+print ()
+
+print (mylist[-1])
+
+mylist[0]="rock"
+
+print()
+
+print(mylist)
+
+del mylist[2]
+
+print()
+
+print(mylist)
+
+myTuple=('add','dad','sad','mad')
+
+print()
+
+myTuple[3]='open'
+
+print(myTuple)
+
+
+
---
+++ loops.py
@@ -0,0 +1,18 @@
+# x=1
+
+# while x!=11:
+#     print(x)
+#     x+=1
+
+
+# print() 
+# print("Part 2")
+# print()
+# groceries=['apple','banna','orange']
+
+# for z in groceries:
+#     print(z)
+
+
+for i in range(1,10):
+    print(i)
