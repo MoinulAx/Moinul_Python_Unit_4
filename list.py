4324407f06dd6a9619f2542eef881c325b0902d6
MoinulAx <113442311+MoinulAx@users.noreply.github.com>
2022-11-16 09:28:51 -0500

    list

--- list.py
+++ list.py
@@ -1,23 +1,31 @@
-
+#Holds the items of the list in the variable
 mylist=["meat","beep",'boop',"meep"]
 
+#Outputs the list
 
 print(mylist)
 
+#Adds an item to our list 
 mylist.append ("ginger")
 
+#outputs a  space
 print()
 
+#Outputs the list
 print(mylist)
 
+#outputs a  space
 print()
 
+#Outputs the list item that is the 3rd
 print (mylist[2])
 
-print ()
+print()
 
+#Outputs the list, specfically the last item
 print (mylist[-1])
 
+#changes the first item of the list to rock
 mylist[0]="rock"
 
 print()
@@ -30,13 +38,78 @@ print()
 
 print(mylist)
 
-myTuple=('add','dad','sad','mad')
+print()
+
+#Tells you the length of any datatype
+print(len(mylist))
 
 print()
 
-myTuple[3]='open'
+print(len("string"))
 
-print(myTuple)
+#The list gets sorted alphebetically
+mylist.sort()
+
+print()
+
+print (mylist)
+
+print()
+
+#The order of the orginal list will then be reversed.
+mylist.reverse()
+
+print(mylist)
+
+secondList=['a','b','c','d']
+
+print()
+
+print(mylist+secondList)
+
+print()
+
+#Allows you to output the list without the bracets and quotations
+print(" ".join(mylist+secondList))
+
+print()
+
+#Makes the string into a list 
+print("mop,bop,clock".split(","))
+
+print()
+
+#The conversion of a string can be then turned in a list through the split method
+names="dop,clock,rack".split(",")
+
+print(names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# myTuple=('add','dad','sad','mad')
+
+# print()
+
+# #myTuple[3]='open'
+
+# print(myTuple)
 
 
 
--- todo.py
+++ todo.py
@@ -3,25 +3,22 @@ todo=[]
 
 
 
+x=input("What do you want to add to your tolist: ")
+todo.append(x)
+print(todo)
+
 
 
 while True:
-    x=input("What do you want to add to your tolist: ")
-    todo.append(x)
-    print(todo)
-
-
-
-
-#   h= input("Is there anything else that you would like to add to your list enter y/n: ")
-#   if h!= "y"or h!="n":
-#     print()
-#     if h=="y":
-#       z="enter more items: "
-#       todo.append(z)
-#     elif h=="n":
-#       print ("great ")
-#   else: 
-#     print("Please enter y or n for Yes or No")
+  h= input("Is there anything else that you would like to add to your list enter y/n: ")
+  if h== "y" or h=="n":
+    print()
+    if h=="y":
+      z=input("enter more items: ")
+      todo.append(z)
+    elif h=="n":
+      print ("great ")
+  else: 
+    print("Please enter y or n for Yes or No")
     
     
