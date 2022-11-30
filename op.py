e78796a4d34a2b79cd80694b1b8f292f6bce2d09
MoinulAx <113442311+MoinulAx@users.noreply.github.com>
2022-11-30 15:45:18 -0500

    j

--- op.py
+++ op.py
@@ -1,5 +1,47 @@
-
-
+from turtle import *
+speed(0)
 # user_int = int(input("Enter an integer: "))
 # for i in range(1, user_int):5
 #     print (i)
+# square_length = 20
+
+# for i in range(4):
+#     forward(square_length)
+#     left(90)
+# square_length = square_length * 2
+
+# square_length = 20
+
+# for i in range(4):
+#     forward(square_length)
+#     left(90)
+# square_length = square_length * 2
+
+
+square_length = 20
+
+for i in range(4):
+    for i in range(4):
+        forward(square_length)
+        left(90)
+        square_length = square_length * 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+done()
