5af6fe15aa041fa3b816acd77c052abf652cd49b
MoinulAx <113442311+MoinulAx@users.noreply.github.com>
2022-12-06 08:41:41 -0500

    huoh

--- op.py
+++ op.py
@@ -1,5 +1,5 @@
-from turtle import *
-speed(0)
+# from turtle import *
+# speed(0)
 # user_int = int(input("Enter an integer: "))
 # for i in range(1, user_int):5
 #     print (i)
@@ -18,13 +18,38 @@ speed(0)
 # square_length = square_length * 2
 
 
-square_length = 20
+# square_length = 20
 
-for i in range(4):
-    for i in range(4):
-        forward(square_length)
-        left(90)
-        square_length = square_length * 2
+# for i in range(4):
+#     for i in range(4):
+#         forward(square_length)
+#         left(90)
+#         square_length = square_length * 2
+
+# i = 5
+# while i < 10:
+#   print(i)
+#   if i == 8:
+#     break
+#   i += 1
+
+# a = 0
+# while a < 5:
+#   if a == 4:
+#     continue
+#   print(a)
+
+comp_brands = ["Apple", "Asus", "Dell", "Samsung"]
+
+i = 0
+
+while i < len(comp_brands):
+  print(comp_brands[i])
+  
+  if i == "dell":
+    continue
+
+  i = i + 1
 
 
 
@@ -44,4 +69,3 @@ for i in range(4):
 
 
 
-done()
